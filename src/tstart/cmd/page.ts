8b4163ffa776337acd695cbd5b2fb0d9c0b4ec46
srnpr <srnpr@sina.com>
2017-12-04 14:26:05 +0800

    a

--- src/tstart/cmd/page.ts
+++ src/tstart/cmd/page.ts
@@ -1,9 +1,9 @@
 import {IargsStart} from './../../air/face/args';
-
-console.log(process.cwd());
-
 import yargs from 'yargs';
 import {TcoreHelperObject} from '../../tcore/index';
+
+
+
 
 let a : IargsStart = TcoreHelperObject.parse<IargsStart>(yargs({}).options({
     'path': {
