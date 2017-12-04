02100b63e5164b2e1bcf571a103d29ce269d0b8f
srnpr <srnpr@sina.com>
2017-12-04 15:20:12 +0800

    c

--- dist/tprogram/vend/cmd.js
+++ dist/tprogram/vend/cmd.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = require("../../tcore/index");
-var program_1 = require("../../air/import/program");
+var program_1 = require("../../air/export/program");
 var VendCmd = (function () {
     function VendCmd() {
     }
     VendCmd.start = function () {
-        var a = index_1.TcoreHelperObject.parseTs(program_1.AprogramYargs({}).options({
+        var a = index_1.TcoreHelperObject.parseTs(program_1.EprogramYargs({}).options({
             'path': {
                 alias: 'p',
                 describe: 'run your program'
