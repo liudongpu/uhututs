db05d429e31bed41783d38af033e6c2785ccb52a
srnprmini <srnpr@srnprminideMini.lan>
2018-04-22 18:43:49 +0800

    abc

--- dist/ulocal/see/see.js
+++ dist/ulocal/see/see.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var default_1 = /** @class */ (function () {
     function default_1() {
     }
-    default_1.prototype.eeee = function () {
+    default_1.prototype.abcdefgh = function () {
     };
     return default_1;
 }());
