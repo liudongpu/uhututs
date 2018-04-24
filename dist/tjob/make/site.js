69cf054ad5f5305f6124266e778cd27359f2e197
srnpr <srnpr@sina.com>
2018-04-24 17:59:15 +0800

    c

---
+++ dist/tjob/make/site.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MakeVue = /** @class */ (function () {
+    function MakeVue() {
+    }
+    return MakeVue;
+}());
+exports.MakeVue = MakeVue;
