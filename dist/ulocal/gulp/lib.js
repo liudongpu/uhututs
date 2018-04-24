5a5d7fd2d56bebe76a29917d8b44555cd6cb5bc9
srnpr <srnpr@sina.com>
2018-04-24 15:28:48 +0800

    c

---
+++ dist/ulocal/gulp/lib.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var tsGulp = require("gulp-typescript");
+//var tsProject = tsGulp.createProject("../../../tsconfig.json");
+var watch = require('gulp-watch');
+var childProcess = require("child_process");
+var tsProject = tsGulp.createProject("lib-tsconfig.json");
+gulp.task("lib:compile", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest(tsProject.options.outDir));
+});
+gulp.task('watch:lib', ['lib:compile'], function () {
+    gulp.watch("src/**/*", ['lib:compile']);
+});
+gulp.task("lib", ["lib:compile"]);
